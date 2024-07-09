aa3b1091f61c71ef1e6ca3cfb78766e63bcf79a4
ShariarNiaj05 <shariarn85@gmail.com>
2024-07-10 00:21:04 +0600

    product interface added

---
+++ src/app/modules/products/products.interface.ts
@@ -0,0 +1,10 @@
+export interface IProduct {
+  productName: string
+  sportsCategory: string
+  stockQuantity: number
+  branch: string
+  rating: number
+  productDescription: string
+  price: number
+  image: string
+}
