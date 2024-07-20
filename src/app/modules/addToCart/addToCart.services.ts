5b8eab6054ef798f84a00a00cafca2af6569433e
ShariarNiaj05 <shariarn85@gmail.com>
2024-07-21 00:44:54 +0600

    concise the cart service

--- src/app/modules/addToCart/addToCart.services.ts
+++ src/app/modules/addToCart/addToCart.services.ts
@@ -1,11 +1,3 @@
-// const getAllProductsIntoDB = async () => {
-//   const result = await Product.find().sort({ createdAt: -1 })
-//   return result
-// }
-// const getSingleProductFromDB = async (id: string) => {
-//   const result = await Product.findById(id)
-//   return result
-
 import { IAddToCart } from './addToCart.interface'
 import { AddToCart } from './addToCart.model'
 
@@ -15,20 +7,7 @@ const insertCartIntoDB = async (payload: IAddToCart) => {
   const result = await AddToCart.create(payload)
   return result
 }
-// const deleteProductFromDB = async (id: string) => {
-//   const result = await Product.findByIdAndDelete(id)
-//   return result
-// }
-// const updateProductIntoDB = async (id: string, payload: Partial<IProduct>) => {
-//   const result = await Product.findByIdAndUpdate(id, payload, { new: true })
-//   return result
-// }
 
 export const AddToCartServices = {
   insertCartIntoDB,
-  // insertProductIntoDB,
-  // deleteProductFromDB,
-  // updateProductIntoDB,
-  // getAllProductsIntoDB,
-  // getSingleProductFromDB,
 }
