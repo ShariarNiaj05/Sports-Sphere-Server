7056c4985896ba86a905bc7de98fa5314d1c62db
ShariarNiaj05 <shariarn85@gmail.com>
2024-07-28 00:17:52 +0600

    multiple file updated

--- src/app/modules/addToCart/addToCart.route.ts
+++ src/app/modules/addToCart/addToCart.route.ts
@@ -3,10 +3,6 @@ import { addToCartController } from './addToCart.controller'
 
 const router = Router()
 
-// router.get('/', ProductController.getProducts)
-// router.get('/:id', ProductController.getSingleProduct)
-// router.post('/add-product', ProductController.insertProduct)
-// router.delete('/delete-product/:id', ProductController.deleteProduct)
 router.put('/add-to-cart', addToCartController.insertToCart)
 
 export const AddToCartRoutes = router
